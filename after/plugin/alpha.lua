0b19a6b0255049cfd76a0b05ac8b5a952c7f4ca8
Thiago <thiago.rpxx@gmail.com>
2025-05-20 20:48:13 -0300

    update-alpha

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -18,10 +18,10 @@ dashboard.section.header.val = {
 -- Set menu
 dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", "<cmd>Telescope find_files<cr>"),
+    dashboard.button( "f", "󰈞  > Find file", "<cmd>Telescope find_files<cr>"),
     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
     dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+    dashboard.button( "q", "󰩈  > Quit NVIM", ":qa<CR>"),
 }
 
 -- Set footer
